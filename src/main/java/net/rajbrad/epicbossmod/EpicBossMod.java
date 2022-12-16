b41cf5ddc9832d06c0e6dada76c0bffa87208093
brandochewitt <109834661+brandochewitt@users.noreply.github.com>
2022-12-16 22:58:38 +0000

    a

--- src/main/java/net/rajbrad/epicbossmod/EpicBossMod.java
+++ src/main/java/net/rajbrad/epicbossmod/EpicBossMod.java
@@ -1,7 +1,6 @@
 package net.rajbrad.epicbossmod;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -9,22 +8,18 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
-@Mod(epicBossMod.MODID)
-public class epicBossMod
+@Mod(EpicBossMod.MODID)
+public class EpicBossMod
 {
     /*
     ░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░
@@ -53,7 +48,7 @@ public class epicBossMod
     public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
     public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
 
-    public epicBossMod()
+    public EpicBossMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
--- src/main/java/net/rajbrad/epicbossmod/event/ClientEvents.java
+++ src/main/java/net/rajbrad/epicbossmod/event/ClientEvents.java
@@ -7,13 +7,11 @@ import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.rajbrad.epicbossmod.epicBossMod;
+import net.rajbrad.epicbossmod.EpicBossMod;
 import net.rajbrad.epicbossmod.util.ModKeyBindings;
 
-import javax.swing.text.JTextComponent;
-
 public class ClientEvents {
-    @Mod.EventBusSubscriber(modid = epicBossMod.MODID, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = EpicBossMod.MODID, value = Dist.CLIENT)
     public static class ClientForgeEvents {
         @SubscribeEvent
         public static void onKeyInput(InputEvent.Key event) {
@@ -23,7 +21,7 @@ public class ClientEvents {
         }
     }
 
-    @Mod.EventBusSubscriber(modid = epicBossMod.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    @Mod.EventBusSubscriber(modid = EpicBossMod.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModBusEvents {
         @SubscribeEvent
         public static void onKeyRegister(RegisterKeyMappingsEvent event) {
